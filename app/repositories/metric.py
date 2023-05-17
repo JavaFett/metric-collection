804d16fc77b3aec8e6b60e6a7c822517d9447aa1
Vadim <52993364+JavaFett@users.noreply.github.com>
2023-05-17 21:32:54 +0500

    Removed comments in metric repository

--- app/repositories/metric.py
+++ app/repositories/metric.py
@@ -19,15 +19,7 @@ class MetricRepository:
         return metric
 
     async def get_metrics_by_service_name(self, service_name: str) -> List[Metric]:
-        #     records = self.session.execute(
-        #         select(Metric.path, func.avg(Metric.response_time_ms),
-        #                func.min(Metric.response_time_ms), func.max(
-        #                    Metric.response_time_ms))
-        #         .where(Metric.service_name == service_name)
-        #         .group_by(Metric.path)
-        #     ).all()
-
         metrics = self.session.query(Metric).filter_by(
-            service_name=service_name).order_by().order_by(Metric.path).all()
+            service_name=service_name).order_by(Metric.path).all()
 
         return metrics
